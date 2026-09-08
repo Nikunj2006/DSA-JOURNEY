0278e27abb4905a2e466ac6d987be851e6eeb11c
Nikunj Bharadiya <163641916+Nikunj2006@users.noreply.github.com>
2026-09-08 22:29:56 +0530

    Time: 4 ms (1.16%), Space: 15.8 MB (54.38%) - LeetHub

---
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -0,0 +1,24 @@
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int n = nums.size();
+        int i = n - 2;
+
+        // 1) find first index from right where nums[i] < nums[i+1]
+        while (i >= 0 && nums[i] >= nums[i + 1]) i--;
+
+        // 2) if found, find just larger element to the right and swap
+        if (i >= 0) {
+            int j = n - 1;
+            while (nums[j] <= nums[i]) j--;
+            swap(nums[i], nums[j]);
+        }
+
+        // 3) reverse the suffix
+        reverse(nums.begin() + i + 1, nums.end());
+    }
+};
