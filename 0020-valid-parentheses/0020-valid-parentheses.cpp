86c42a4e71fdc134d3e1a270c6623f68f266a29f
Nikunj Bharadiya <163641916+Nikunj2006@users.noreply.github.com>
2026-09-10 20:55:22 +0530

    Time: 0 ms (100%), Space: 8.8 MB (67.19%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -8,17 +8,17 @@ public:
         stack<char> st;
 
         for (char c : s) {
-            // push opening brackets
+            
             if (c == '(' || c == '{' || c == '[') {
                 st.push(c);
             } else {
-                // if stack empty → invalid
+                
                 if (st.empty()) return false;
 
                 char top = st.top();
                 st.pop();
 
-                // check matching pair
+                
                 if ((c == ')' && top != '(') ||
                     (c == '}' && top != '{') ||
                     (c == ']' && top != '[')) {
@@ -27,6 +27,6 @@ public:
             }
         }
 
-        return st.empty(); // must be empty if valid
+        return st.empty(); 
     }
 };
